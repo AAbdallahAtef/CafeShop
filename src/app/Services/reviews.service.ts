845b406ff365d6a67f7e0d2362da1d5df4496823
Abdallah Atef <abdallaatef12@google.com>
2024-04-07 03:10:27 +0200

    final step

--- src/app/Services/reviews.service.ts
+++ src/app/Services/reviews.service.ts
@@ -5,7 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ReviewsService {
-  private DB_URL='http://localhost:3000'
+  private DB_URL='https://cafeshop-data.onrender.com/'
   constructor(private http:HttpClient) { }
   GetReviews(){
     return this.http.get(`${this.DB_URL}/Reviews`)
--- src/app/Services/user.service.ts
+++ src/app/Services/user.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
   constructor(private http:HttpClient) { }
-  private DB_URL =  "http://localhost:3000/users"
+  private DB_URL =  "https://cafeshop-data.onrender.com/"
   getAllUsers(){
     return this.http.get(this.DB_URL);
   }
